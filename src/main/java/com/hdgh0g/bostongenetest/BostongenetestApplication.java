5ddc54c6db7b1925971e1b60d8fb20954ec83870
Hdgh0g <krlv.alxndr@gmail.com>
2018-09-08 15:07:39 +0300

    Interfaces for translation

--- src/main/java/com/hdgh0g/bostongenetest/BostongenetestApplication.java
+++ src/main/java/com/hdgh0g/bostongenetest/BostongenetestApplication.java
@@ -2,8 +2,10 @@ package com.hdgh0g.bostongenetest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class BostongenetestApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/hdgh0g/bostongenetest/service/HystrixSafeTranslationService.java
@@ -0,0 +1,46 @@
+package com.hdgh0g.bostongenetest.service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Primary
+@RequiredArgsConstructor
+@Service("safeTranslationService")
+public class HystrixSafeTranslationService implements TranslationService {
+
+    private static final String TIMEOUT = "5000";
+    private static final String TIMEOUT_PROPERTY = "execution.isolation.thread.timeoutInMilliseconds";
+
+    private final TranslationService translationService;
+
+    @Override
+    @HystrixCommand(
+            fallbackMethod = "detectLanguageFallback",
+            commandProperties = @HystrixProperty(name = TIMEOUT_PROPERTY, value = TIMEOUT)
+    )
+    public Optional<String> detectLanguage(String text) {
+        return translationService.detectLanguage(text);
+    }
+
+    @Override
+    @HystrixCommand(
+            fallbackMethod = "translateTextFallback",
+            commandProperties = @HystrixProperty(name = TIMEOUT_PROPERTY, value = TIMEOUT)
+    )
+    public Optional<String> translateText(String text, String sourceLanguage, String targetLanguage) {
+        return translationService.translateText(text, sourceLanguage, targetLanguage);
+    }
+
+    private Optional<String> detectLanguageFallback(String text) {
+        return Optional.empty();
+    }
+
+    private Optional<String> translateTextFallback(String text, String sourceLanguage, String targetLanguage) {
+        return Optional.empty();
+    }
+}
