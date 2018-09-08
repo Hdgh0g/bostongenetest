46bcfc62669f9f137607a2b041653172a360cee5
Hdgh0g <krlv.alxndr@gmail.com>
2018-09-08 09:16:20 +0300

    InitialCommit

---
+++ src/main/java/com/hdgh0g/bostongenetest/BostongenetestApplication.java
@@ -0,0 +1,12 @@
+package com.hdgh0g.bostongenetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BostongenetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BostongenetestApplication.class, args);
+	}
+}
